a36cc6a44214d744013616f5fdcd2a7ffaa8560d
sydbassett <luka.vader@gmail.com>
2021-10-25 20:37:00 +0200

    - HomeListViewController renamed to HomeViewController

---
+++ HomegateTestAssignmentUIKit/HomegateTestAssignmentUIKit/_src/Scenes/TabMenu/Home/_container/List/HomeListView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeListView.swift
+//  HomegateTestAssignmentUIKit
+//
+//  Created by Vladimir Lukic on 25.10.21..
+//
+
+import UIKit
+
+class HomeListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ HomegateTestAssignmentUIKit/HomegateTestAssignmentUIKit/_src/Scenes/TabMenu/Home/_container/Map/HomeMapView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeMapView.swift
+//  HomegateTestAssignmentUIKit
+//
+//  Created by Vladimir Lukic on 25.10.21..
+//
+
+import UIKit
+
+class HomeMapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
